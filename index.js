529a738ab068323564129c2dd455086640212775
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-07 15:06:51 +0100

    refactor POST /upload endoing and websocket to use getFileInformation function

--- index.js
+++ index.js
@@ -13,47 +13,43 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.get('/upload', async (req, res) => {
-    try {
-      const fileInfos = await getFileInformation(path.join(__dirname, 'uploads'));
-      res.json({ files: fileInfos });
-    } catch (err) {
-      console.error("Error getting file stats:", err);
-      res.status(500).json({ error: "Error getting file stats" });
-    }
-  });
-app.post("/upload", upload.single("file"), (req, res) => {
+app.get("/upload", async (req, res) => {
+  try {
+    const fileInfos = await getFileInformation(path.join(__dirname, "uploads"));
+    res.json({ files: fileInfos });
+  } catch (err) {
+    console.error("Error getting file stats:", err);
+    res.status(500).json({ error: "Error getting file stats" });
+  }
+});
+app.post("/upload", upload.single("file"), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded" });
   }
 
   const fileUrl = `/uploads/${req.file.filename}`;
 
-  fs.readdir("uploads", (err, files) => {
-    if (err) {
-      return res.status(500).send("Error reading uploads folder.");
-    }
-
-    const fileUrls = files.map((file) => `/uploads/${file}`);
-    io.emit("filesUploaded", fileUrls);
-    res.status(201).json({ fileUrl,message:"File uploaded successfully" });
-  });
+  try {
+    const fileInfos = await getFileInformation(path.join(__dirname, "uploads"));
+    io.emit("filesUploaded", fileInfos);
+    res.json({ fileUrl });
+  } catch (err) {
+    console.error("Error getting file stats:", err);
+    res.status(500).send("Error getting file stats.");
+  }
 });
 
 let currentClipboardData = "";
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("A user connected");
   socket.emit("clipboard", currentClipboardData);
-  fs.readdir("uploads", (err, files) => {
-    if (err) {
-      console.error("Error reading uploads folder.");
-      return;
-    }
-
-    const fileUrls = files.map((file) => `/uploads/${file}`);
-    socket.emit("filesUploaded", fileUrls);
-  });
+  try {
+    const fileInfos = await getFileInformation(path.join(__dirname, "uploads"));
+    socket.emit("filesUploaded", fileInfos);
+  } catch (err) {
+    console.error("Error getting file stats:", err);
+  }
 
   socket.on("clipboard", (data) => {
     console.log("Clipboard data received:", data);
