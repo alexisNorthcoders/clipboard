48fa7bc6b24cce32c98cb8866b5bf4028b764fd1
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-07 12:04:29 +0100

    remove listenning code and export the server

--- index.js
+++ index.js
@@ -47,7 +47,4 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4123;
-server.listen(PORT, () => {
-  console.log(`Clipboard Server is running on port ${PORT}`);
-});
+module.exports = server;
