6a4a3894056bf88226f0eb545ed3368202cb5bb2
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-07 11:35:39 +0100

    add /upload endpoint to allow for file sharing

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static("public"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-let currentClipboardData = '';
+app.post("/upload", (req, res) => {
+  if (!req.files || !req.files.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  const file = req.files.file;
+
+  file.mv(path.join(__dirname, "uploads", file.name), (err) => {
+    if (err) {
+      return res.status(500).json({ error: "File upload failed" });
+    }
+
+    const fileUrl = `/uploads/${file.name}`;
+    io.emit("file", fileUrl);
+    res.status(200).json({ fileUrl });
+  });
+});
+
+let currentClipboardData = "";
 
 io.on("connection", (socket) => {
   console.log("A user connected");
-  socket.emit('clipboard', currentClipboardData);
+  socket.emit("clipboard", currentClipboardData);
 
   socket.on("clipboard", (data) => {
     console.log("Clipboard data received:", data);
