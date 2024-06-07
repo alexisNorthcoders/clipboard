48584d0fbf5c485b905f8934f1b80e7a9dccf3fc
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-07 12:11:07 +0100

    add test when request is made without a file

--- __tests__/upload.test.js
+++ __tests__/upload.test.js
@@ -1,29 +1,31 @@
-const request = require('supertest');
-const fs = require('fs');
-const path = require('path');
-const server = require('../index');
+const request = require("supertest");
+const fs = require("fs");
+const path = require("path");
+const server = require("../index");
 
-describe('File Upload', () => {
-  it('should upload a file', async () => {
-    
-    const sampleFilePath = path.join(__dirname, 'sample.txt');
-    fs.writeFileSync(sampleFilePath, 'Sample text file content');
+describe("File Upload", () => {
+  it("should upload a file", async () => {
+    const sampleFilePath = path.join(__dirname, "sample.txt");
+    fs.writeFileSync(sampleFilePath, "Sample text file content");
 
-   
     const response = await request(server)
-      .post('/upload')
-      .attach('file', sampleFilePath);
+      .post("/upload")
+      .attach("file", sampleFilePath);
 
-  
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('fileUrl');
+    expect(response.body).toHaveProperty("fileUrl");
 
     fs.unlinkSync(sampleFilePath);
   });
-});
-describe('Server', () => {
-    it('should return status code 200', async () => {
-      const response = await request(server).get('/');
-      expect(response.status).toBe(200);
-    });
+  it("should error 400 if file not uploaded", async () => {
+    const response = await request(server).post("/upload");
+
+    expect(response.status).toBe(400);
   });
+});
+describe("Server", () => {
+  it("should return status code 200", async () => {
+    const response = await request(server).get("/");
+    expect(response.status).toBe(200);
+  });
+});
