553c52c3807eaa661ab95edd53db8a509a8f7c1f
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-10 22:24:34 +0100

    implement SQlite database

---
+++ database.js
@@ -0,0 +1,25 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./DB/database.sqlite");
+
+console.log("Clearing and recreating database...")
+db.serialize(() => {
+   
+  db.run("DROP TABLE IF EXISTS users", function(err) {
+    if (err) {
+      console.error("Error dropping users table", err.message);
+      return;
+    }
+    console.log("Users table dropped.");
+
+    db.run(
+      "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, password TEXT)",
+      function(err) {
+        if (err) {
+          console.error("Error creating users table", err.message);
+          return;
+        }
+        console.log("Users table created.");
+      }
+    );
+  });
+});
