1322ac13510d459c1ec0020adc0404335fb1986a
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-10 17:07:19 +0100

    refactor to get positive response webhook is triggered

--- index.js
+++ index.js
@@ -8,29 +8,27 @@ const { getFileInformation } = require("./utils");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
-const crypto = require('crypto');
-const secret = process.env.SECRET
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
+const crypto = require("crypto");
+const secret = process.env.SECRET;
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use('/webhook', bodyParser.json({ verify: verifySignature }));
+app.use("/webhook", bodyParser.json({ verify: verifySignature }));
 
-app.post('/webhook', (req, res) => {
-  
-  exec('./script/deploy.sh', (error, stdout, stderr) => {
+app.post("/webhook", (req, res) => {
+  res.status(200).send("Deployment script is being executed.");
+
+  exec("./script/deploy.sh", (error) => {
     if (error) {
       console.error(`exec error: ${error}`);
-      return res.status(500).send(`Deployment script failed: ${stderr}`);
     }
-    console.log(`stdout: ${stdout}`);
-    res.status(200).send('Deployment script executed successfully.');
   });
 });
-app.get("/webhook",(req,res)=>{
-  res.status(200).send('Making sure webhook is working correctly.');
-})
+app.get("/webhook", (req, res) => {
+  res.status(200).send("Making sure webhook is working correctly.");
+});
 
 app.get("/upload", async (req, res) => {
   try {
@@ -50,7 +48,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   try {
     const fileInfos = await getFileInformation(path.join(__dirname, "uploads"));
     io.emit("filesUploaded", fileInfos);
-    res.status(201).json({ fileUrl,message:"File uploaded successfully" });
+    res.status(201).json({ fileUrl, message: "File uploaded successfully" });
   } catch (err) {
     console.error("Error getting file stats:", err);
     res.status(500).send("Error getting file stats.");
@@ -82,9 +80,12 @@ io.on("connection", async (socket) => {
 });
 
 function verifySignature(req, res, buf) {
-  const signature = `sha256=${crypto.createHmac('sha256', secret).update(buf).digest('hex')}`;
-  if (req.headers['x-hub-signature-256'] !== signature) {
-    throw new Error('Invalid signature.');
+  const signature = `sha256=${crypto
+    .createHmac("sha256", secret)
+    .update(buf)
+    .digest("hex")}`;
+  if (req.headers["x-hub-signature-256"] !== signature) {
+    throw new Error("Invalid signature.");
   }
 }
 
