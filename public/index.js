d9d306b52090474c6428fe4cee93a2c94fc5f5df
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-12 16:42:57 +0100

    fix dynamic tailwindcss classes not being rendered, added to the safelist in config file

--- public/index.js
+++ public/index.js
@@ -119,5 +119,3 @@ document.addEventListener("paste", function (event) {
     }
   }
 });
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     './public/index.html',
     './src/**/*.{html,js}',
   ],
+  safelist: [
+   
+    'bg-red-500', 'bg-blue-500', 'bg-green-500', 
+    'duration-300', 'duration-500', 
+   
+    {
+      pattern: /bg-(red|green|blue)-(100|200|300|400|500)/,
+      variants: ['hover', 'focus', 'active'],
+    },
+    {
+      pattern: /duration-(300|500|700|1000)/,
+    },
+  ],
   theme: {
     extend: {},
   },
