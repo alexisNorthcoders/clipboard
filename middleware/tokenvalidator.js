a05d323ccf8b0b62bb79998460f37b3c88f91f85
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-16 00:04:54 +0100

    create function to generate a test token for testing

--- middleware/tokenvalidator.js
+++ middleware/tokenvalidator.js
@@ -1,25 +1,29 @@
 const jwt = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
-    try {
-        let token;
-        let authHeader = req.headers.Authorization || req.headers.authorization;
-        if (authHeader && authHeader.startsWith("Bearer")) {
-            token = authHeader.split(" ")[1];
-            jwt.verify(token, process.env.DATABASE_SECRET, (err, decoded) => {
-                if (err) {
-                    res.status(401);
-                    throw new Error("User is not authorized");
-                }
-                req.user = decoded.user;
-                next();
-            });
-        } else {
-            res.status(401).send({message:"User is not authorized or token is missing"});
-            return
+  try {
+    let token;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer")) {
+      token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.DATABASE_SECRET, (err, decoded) => {
+        if (err) {
+          res.status(401);
+          throw new Error("User is not authorized");
         }
-    } catch (error) {
-        next(error);
+        req.user = decoded.user;
+        next();
+      });
+    } else {
+      res.status(401).send({ message: "User is not authorized or token is missing" });
+      return;
     }
+  } catch (error) {
+    next(error);
+  }
 };
-module.exports= {validateToken};
+const generateTestToken = () => {
+  const user = { id: "testUser" }; 
+  return jwt.sign(user, process.env.DATABASE_SECRET, { expiresIn: "1h" });
+};
+module.exports = { validateToken, generateTestToken };
