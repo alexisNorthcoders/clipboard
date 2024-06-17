0c876996bfce27f56dc78026862868997ed64942
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-17 11:27:31 +0100

    impletement a deletionQueue, to automatically remove files after a set time using redis and Bull library

---
+++ deletionQueue.js
@@ -0,0 +1,27 @@
+const Queue = require("bull");
+const path = require("path");
+const fs = require("fs");
+const { removeFileFromMap } = require("./utils");
+
+const fileDeletionQueue = new Queue("fileDeletion", {
+  redis: {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+});
+
+fileDeletionQueue.process(async (job) => {
+  const { filePath, userId, filename } = job.data;
+  console.log(job.data)
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(`Error deleting file ${filePath}:`, err);
+    } else {
+      console.log(`File ${filePath} deleted successfully after 5 seconds.`);
+
+      removeFileFromMap(userId,filename)
+    }
+  });
+});
+
+module.exports = fileDeletionQueue;
